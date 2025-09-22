15995b513fd32033b2697631cea08e2913867f59
Nouman Khan <nomiofficial68@gmail.com>
2025-09-22 23:10:15 +0500

    feat: add register api route

---
+++ app/api/auth/signup/route.ts
@@ -0,0 +1,50 @@
+import { connectDB } from "@/lib/db";
+import { User } from "@/models/user.model";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+
+export async function POST(request: NextRequest) {
+  try {
+    const { email, password } = await request.json();
+
+    if (!email || !password) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Email and password are required",
+        },
+        { status: 400 }
+      );
+    }
+
+    await connectDB();
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return NextResponse.json(
+        { success: false, message: "Email already exists" },
+        { status: 400 }
+      );
+    }
+
+    const newUser = await User.create({
+      email,
+      password,
+    });
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: "User registered successfully",
+        data: { email: newUser.email },
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { success: false, message: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
