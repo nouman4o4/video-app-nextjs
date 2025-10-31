7bdf9ec61dcdd9d2610c79431ef5cd098a91fefa
Nouman Khan <nomiofficial68@gmail.com>
2025-10-31 22:49:39 +0500

    feat: update registration schema to include firstname and lastname; and add social icons svgs

--- schemas/register.schema.ts
+++ schemas/register.schema.ts
@@ -1,9 +1,21 @@
 import { TypeOf, z } from "zod"
 
 export const registerSchema = z.object({
-  name: z.string().min(3, "Name must be at least 3 characters."),
-  email: z.string().email("Invalid email adress."),
-  password: z.string().min(6, "Password should be more than 6 chars"),
+  firstname: z
+    .string()
+    .min(3, "First name must be at least 3 characters.")
+    .max(10, "First name shoud be less than 10 chars."),
+  lastname: z
+    .string()
+    .min(3, "Last name must be at least 3 characters.")
+    .max(10, "Last name shoud be less than 10 chars."),
+  email: z
+    .string({ error: "Email is required" })
+    .email("Invalid email adress."),
+  password: z
+    .string()
+    .min(6, "Password should be more than 6 chars")
+    .max(15, "Passwod shloud be less than 15 chars"),
 })
 
 export type registerFormData = z.infer<typeof registerSchema>
