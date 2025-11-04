d261cf42a5ccb9c9bda3baa6f5219d0811d826fd
Nouman Khan <nomiofficial68@gmail.com>
2025-11-04 23:35:37 +0500

    fix: update max length validation for firstname and lastname in user update schema

--- schemas/userUpdateSchema.ts
+++ schemas/userUpdateSchema.ts
@@ -4,11 +4,11 @@ export const userUpdateSchema = z.object({
   firstname: z
     .string()
     .min(3, "First name must be at least 3 characters.")
-    .max(10, "First name shoud be less than 10 chars."),
+    .max(20, "First name shoud be less than 10 chars."),
   lastname: z
     .string()
     .min(3, "Last name must be at least 3 characters.")
-    .max(10, "Last name shoud be less than 10 chars."),
+    .max(20, "Last name shoud be less than 10 chars."),
   about: z
     .string()
     .max(100, "About details should not be more than 100 chars")
