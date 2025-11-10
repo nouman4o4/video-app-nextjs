17ecca5f4e532d2f8ff8f7acf1c4a9906877c9f0
Nouman Khan <nomiofficial68@gmail.com>
2025-11-11 01:20:11 +0500

    feat: add project status label

---
+++ app/components/Label.tsx
@@ -0,0 +1,9 @@
+export default function Label() {
+  return (
+    <div className="p-8 fixed bottom-5 left-5 border-2 border-yellow-500 h-20 bg-white rounded-xl flex items-center justify-center shadow-xl  ">
+      <p className="text-center text-xl font-bold text-yellow-400">
+        This project is in progress
+      </p>
+    </div>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css"
 import { Toaster } from "react-hot-toast"
 import Providers from "./providers/Providers"
 import Navbar from "./components/Navbar"
+// import PinterestSidebar from "./components/Sidebar"
+import Label from "./components/Label"
 
 // const geistSans = Geist({
 //   variable: "--font-geist-sans",
@@ -31,7 +33,12 @@ export default function RootLayout({
         <Toaster />
         <Providers>
           <Navbar />
+          {/* <PinterestSidebar /> */}
+
           <div className="mt-16">{children}</div>
+          <div className="fixed top-4 right-4 z-50">
+            <Label />
+          </div>
         </Providers>
       </body>
     </html>
